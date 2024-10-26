a3043645fa77a800789857078d97e36d049b6972
SUCI <suciffdyy@gmail.com>
2024-10-27 06:51:05 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "SUCI FITRIYAH DARMAYANTI" // TODO: replace this
+const studentId = "FS12194279" // TODO: replace this
+
+export { url, studentName, studentId }
